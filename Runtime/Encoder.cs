7263c52e06dd440892fbe1bd00c75273cd2685f9
Oleg <gthxbr2004@gmail.com>
2025-06-13 21:13:38 +0300

    Restructure project as unity package.

---
+++ Runtime/Encoder.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Encoder
+{
+    public TilemapBuffer tilemapBuffer;
+    public List<Vector2Int> observationCoords;
+    public List<TilemapSensorComponent.TileType> tileTypes;
+
+    public float[] result;
+
+    public Encoder(TilemapBuffer tilemapBuffer, List<Vector2Int> observationCoords, List<TilemapSensorComponent.TileType> tileTypes)
+    {
+        this.tilemapBuffer = tilemapBuffer;
+        this.observationCoords = observationCoords;
+        this.tileTypes = tileTypes;
+
+        result = new float[(tileTypes.Count + tilemapBuffer.NumChannels) * observationCoords.Count];
+    }
+
+    public float[] Encode()
+    {
+        int c = 0;
+
+        foreach (float tile in tilemapBuffer.GetChannel(0))
+            for (int i = 0; i < tileTypes.Count + 1; i++)
+                result[c++] = (i == tile) ? 1 : 0;
+
+        for (int i = 1; i < tilemapBuffer.NumChannels; i++)
+        foreach (float data in tilemapBuffer.GetChannel(i))
+            result[c++] = data;
+
+        return result;
+    }
+
+
+
+}
